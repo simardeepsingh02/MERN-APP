a4b6197210845cdc961bf3329f8c024faccb5851
simardeepsingh02 <iamsimar12@gmail.com>
2024-01-25 20:30:02 +0530

    backend

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,20 @@ const mongoose = require("mongoose");
 dotenv.config();
 const userRoute = require("./routes/userRoute");
 
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://astro-y23d.onrender.com/');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+// Your routes and other middleware here...
+
+// app.listen(5000, () => {
+//   console.log('Server is running on port 3000');
+// });
+
 mongoose.connect(process.env.URI).
 then(()=>{
     console.log("connected successfully");
