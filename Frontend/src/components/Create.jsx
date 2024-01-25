c0f70c373299f5862832c5f1c603b4a5fec526dd
simardeepsingh02 <iamsimar12@gmail.com>
2024-01-25 20:26:37 +0530

    new

--- Frontend/src/components/Create.jsx
+++ Frontend/src/components/Create.jsx
@@ -1,7 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Create=()=>{
-    return <div>Create</div>
+const Create = () => {
+  const [fname, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState(0);
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    var addUser = { fname, email, age };
+    console.log(addUser);
+
+    const response = await fetch("https://astro-y23d.onrender.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(addUser),
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      console.log(result.error);
+      setError(result.error);
+    }
+    if (response.ok) {
+      console.log(result);
+      setName("");
+      setEmail("");
+      setAge(0);
+      setError("");
+      navigate("/read");
+    }
+  };
+
+  return (
+    <div className="container my-2">
+      <h1 className="h1 text-center">Fill the data</h1>
+
+      {error && <div className="alert alert-danger"> {error} </div>}
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label className="form-label">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            value={fname}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Age</label>
+          <input
+            type="number"
+            className="form-control"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
