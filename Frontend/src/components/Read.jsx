814d38e1e2ca7cd5c11c2ecb8fcf6d0144b77825
simardeepsingh02 <iamsimar12@gmail.com>
2024-01-25 22:11:20 +0530

    read

--- Frontend/src/components/Read.jsx
+++ Frontend/src/components/Read.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Read=()=>{
-    return <div>Read</div>
+const Read = () => {
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+
+  async function getData() {
+    const response = await fetch("https://astro-y23d.onrender.com/");
+    const result = await response.json();
+    console.log("result..", result);
+    if (!response.ok) {
+      setError(result.error);
+    }
+
+    if (response.ok) {
+      console.log(response.ok);
+      setData(result);
+      setError("");
+    }
+  }
+
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className="container my-2">
+      {error && <div className="alert alert-danger"> {error} </div>}
+      <div className="row">
+        {data?.map((ele) => (
+          <div key={ele._id} className="col-3">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{ele.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{ele.email}</h6>
+                <p className="card-text">{ele.age}</p>
+                <span className="card-link">Edit</span>
+
+                <span className="card-link">Delete</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Read;
