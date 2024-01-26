b72926d9809261d9b6b83c433d3c08c42d177319
simar30122000 <iamsimar12@gmail.com>
2024-01-26 15:01:46 +0530

    xxx

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 //import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import 'bootstrap/dist/js/bootstrap.bundle.min'; // Import Bootstrap JS
+import './../node_modules/bootstrap/dist/js/bootstrap.bundle.min'; // Import Bootstrap JS
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
